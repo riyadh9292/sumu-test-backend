cc3a84b255be62d7a56ca97734ed52da894598ac
riyadh <mrriyadh66@gmail.com>
2023-01-27 19:32:23 +0600

    updated.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,7 +8,7 @@ const {
 const router = express.Router();
 module.exports = router;
 
+router.get("/user", getAllUsers);
 router.post("/user", saveAUser);
 router.delete("/user/:id", deleteAUser);
 router.patch("/user/:id", updateAUser);
-router.get("/user", getAllUsers);
