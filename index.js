ac97fa1466f5acd2eae09a0b5139309838e73778
riyadh <mrriyadh66@gmail.com>
2023-01-27 23:29:43 +0600

    access allow cross origin header added .

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const app = express();
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const userRoutes = require("./routes/user.routes");
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000",
+    "https://sumu-test-frontend.vercel.app/"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/", userRoutes);
